da65c48bde571537dfa3c15ca0e6685ff6878710
Jack Coughlin <johnbcoughlin@gmail.com>
2014-07-31 13:49:33 -0700

    Adds examples folder

---
+++ examples/Simple.hs
@@ -0,0 +1,9 @@
+{-
+- Simple example system -- robot on an 8x8 grid, trying to reach the bottom right.
+- The propositional variables are the position of the robot.
+-}
+
+import MuCalc
+
+--TODO implement the example
+main = do print 46
